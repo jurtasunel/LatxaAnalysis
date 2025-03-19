bad2f2cf8893822a365183cbad52097cb0387b5a
jurtasunel <114501562+jurtasunel@users.noreply.github.com>
2025-03-19 14:21:42 +0100

    Update roncalML.R

--- roncalML.R
+++ roncalML.R
@@ -1,4 +1,3 @@
-
 ### Libraries.
 library(caret)
 library(class)
@@ -43,8 +42,38 @@ setwd("C:/Users/zir826/Desktop/2024-25/other/roncal")
 # # Get rows with valgen and litros acumulados, and remove parent IDs and mostly empty columns.
 # cleanData <- completeData[-c(which(completeData$valgen == "" | completeData$acumPROM == "")), -c(4,5,9)]
 # rownames(cleanData) <- 1:nrow(cleanData)
-# write.csv(completeData, "cleanData2024.csv")
-
+# write.csv(cleanData, "cleanData2024.csv")
+cleanData <- read.csv("cleanData2024.csv")
 
 ### Plotting valgen barplot.
+
+# Gráfico
+# Ensure crotal is a factor and acumPROM is numeric
+cleanData$crotal <- as.factor(cleanData$crotal)
+cleanData$acumPROM <- as.numeric(cleanData$acumPROM)
+
+# Create the bar plot
+p <- ggplot(cleanData, aes(x = crotal, y = acumPROM, fill = as.factor(npartosULT))) +
+  geom_bar(stat = "identity", color = "black", size = 0.2) + # Bordes finos negros en las barras.
+  theme_minimal() +
+  labs(x = "Crotal", y = "Litros acumulados", fill = "Partos") + 
+  scale_fill_brewer(palette = "Set3") +
+  geom_segment(aes(x = crotal, xend = crotal, y = -40, yend = 0), color = "grey8", linewidth = 0.3) + # Lineas de cada barra a us crotal. 
+  theme(
+    panel.grid.major.x = element_blank(),   # Quitar las líneas de cuadrícula en el eje x
+    panel.grid.minor.x = element_blank(),   # Quitar las líneas menores en el eje x
+    panel.grid.major.y = element_line(color = "gray80")  # Hacer más claras las líneas del eje y
+  ) +
+  geom_text(aes(label = valgen), size = 2.5, angle = 60,  nudge_y = 30) + # Texto sobre las barras.
+  geom_label(data = subset(cleanData, valgen > 30), # Markador sobre threshold.
+             aes(label = valgen), fill = "white", color = "blue4", 
+             size = 2.5, angle = 50, nudge_y = 30, label.size = 0.1) +
+  theme(axis.title.x=element_blank(), # Quitar labels del eje x para pltoearlas a parte.
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank()) +
+  geom_text(aes(label = crotal, y = -50),  # Ajustar y según el rango de litros
+            angle = 90, size = 2, vjust = 0.5, hjust = 1)
+
+# Print the plot
+p
 
